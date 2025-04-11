237679183f554754ee6989dc77a71a5d8410a245
DnyaneshwarKalwale <your-email@example.com>
2025-04-11 13:24:19 +0530

    frontend

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,10 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [
-    react(),
+    react({
+      jsxImportSource: 'react',
+      include: "**/*.tsx",
+    }),
     mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
